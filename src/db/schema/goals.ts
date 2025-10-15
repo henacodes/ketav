ba8c5022c0f5a45d004e42c6512eddf6f88ee6ac
kirakos <henacodes@gmail.com>
2025-10-15 11:27:05 +0300

    ...

--- src/db/schema/goals.ts
+++ src/db/schema/goals.ts
@@ -1,8 +1,11 @@
 import { integer, text } from "drizzle-orm/sqlite-core";
 import { sqliteTable } from "drizzle-orm/sqlite-core";
 import { books } from "./book";
+import { InferSelectModel } from "drizzle-orm";
+import { Dialog } from "@radix-ui/react-dialog";
 
 export const dailyReadingGoal = sqliteTable("daily_reading_goal", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
   minutesToRead: integer("minutes_to_read").notNull(),
   associatedBook: integer("associated_book").references(() => books.bookId, {
     onDelete: "cascade",
